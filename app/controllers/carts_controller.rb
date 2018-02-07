8f7befaf95be00d5e8c37ac13426dfd87c39f049
Michael Anderson <michanders@msn.com>
2018-02-07 20:10:07 +0000

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,23 @@
 class CartsController < ApplicationController
+  before_action :set_cart, only: [:show, :checkout]
 
+  def show
+  end
+
+  def checkout
+    @cart.items.each do |item|
+      item.inventory -= @cart.line_items.find_by_id(item.id).quantity
+      item.save
+    end
+    @cart.tap do |cart|
+      cart.user.current_cart.destroy.save
+      redirect_to cart_path(cart)
+    end
+  end
+
+  private
+
+  def set_cart
+    @cart = Cart.find_by_id(params[:id])
+  end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,31 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
+  belongs_to :user
 
+  def add_item(item_id)
+    set_item(item_id)
+    if items.include?(@item)
+      line_item = LineItem.find(@item.id)
+      line_item.tap { |l| l.quantity += 1}.save
+      line_item
+    else
+      line_items.new(cart_id: id, item_id: @item.id)
+    end
+  end
+
+  def total
+    i = 0
+    items.each do |item|
+      quantity = LineItem.find(item.id).quantity
+      i += item.price * quantity
+    end
+    i
+  end
+
+  private
+
+  def set_item(item)
+    @item = Item.find_by_id(item)
+  end
 end
