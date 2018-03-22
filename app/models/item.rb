67a644038373d49b4becc053cd9fe160c67dafc3
Mariel Segovia <msegovia94@gmail.com>
2018-03-22 20:10:10 +0000

    Done.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,17 @@
 class Item < ActiveRecord::Base
 
+  belongs_to :category
+  has_many :line_items
+  has_many :carts, through: :line_items
+
+  def self.available_items
+      where('inventory > ?', 0)
+  end
+
+  def self.update_inventory(cart)
+      cart.line_items.each do |line_item|
+        line_item.item.inventory -= line_item.quantity
+        line_item.item.save
+      end
+  end
 end
