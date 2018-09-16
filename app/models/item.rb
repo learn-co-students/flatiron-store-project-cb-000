d9d68bb7fa9303cf252bd927c745917acce32087
Quinn Dizon <qdizon@gmail.com>
2018-09-16 18:21:31 +0000

    working

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
+
 class Item < ActiveRecord::Base
-  class Item < ActiveRecord::Base
-    belongs_to :category
-    has_many :line_items
+  belongs_to :category
+  has_many :line_items
 
-    def self.available_items
-      where('inventory > ?', 0)
-    end
-
-    def remove(amount)
-      update(inventory: inventory - amount)
-    end
+  def self.available_items
+    where('inventory > ?', 0)
   end
+
+  def remove(amount)
+    update(inventory: inventory - amount)
+  end
+
+
 end
