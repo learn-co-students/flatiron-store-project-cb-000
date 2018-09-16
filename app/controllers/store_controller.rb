72c43689733e10df95f7f2da691e417384ada5ee
Quinn Dizon <qdizon@gmail.com>
2018-09-16 20:32:12 +0000

    working. Store Spec Complete

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
   def index
+    # binding.pry
+    if current_user
+      @user = current_user
+    end
     @categories = Category.all
     @items = Item.available_items
 
