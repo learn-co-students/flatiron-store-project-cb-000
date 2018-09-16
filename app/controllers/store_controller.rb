496e1b5a4e63299fe1816e500ca351ff8977b78f
Quinn Dizon <qdizon@gmail.com>
2018-09-16 21:28:41 +0000

    working

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,6 +3,8 @@ class StoreController < ApplicationController
     # binding.pry
     if current_user
       @user = current_user
+      @user.current_cart ||= @user.create_current_cart
+      @cart = @user.current_cart
     end
     @categories = Category.all
     @items = Item.available_items
